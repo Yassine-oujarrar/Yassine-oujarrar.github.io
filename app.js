e2a6cb1b98c742f637f927e40999901d4c283b5e
Oujarrar <oujarrar2001@gmail.com>
2024-01-09 17:57:43 +0100

    feat: adding icons to every skill and starting the progress bar animation when its visible.

---
+++ app.js
@@ -0,0 +1,18 @@
+function startAnimationWhenVisible(entries) {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add('start-animation'); // Ajoute une classe pour déclencher l'animation
+        }
+    });
+}
+
+const observer = new IntersectionObserver(startAnimationWhenVisible, {
+    root: null,
+    threshold: 0.5, 
+});
+
+const skillContents = document.querySelectorAll('.skills-content');
+
+skillContents.forEach(skill => {
+    observer.observe(skill);
+});
